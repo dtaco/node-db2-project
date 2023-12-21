96a0f175cf123a3e42f7760281968e8863cbad95
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-21 13:52:04 -0500

    created seed file

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567890987',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 132567,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '0987654321234',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 200890,
+        title: 'salvage',
+    },
+    {
+        vin: '2345678909876',
+        make: 'ford',
+        model: 'f150',
+        mileage: 57890,
+    },
+]
+
+exports.seed = function (knex) {
+    return knex('cars')
+        .truncate().then(() => {
+            return knex('cars').insert(cars)
+    })
+}
