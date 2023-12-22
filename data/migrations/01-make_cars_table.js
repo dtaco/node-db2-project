d35955a9ab64356b83d1aca4090ee1220f869912
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-22 07:53:27 -0500

    migration exports up is complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,14 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).notNullable().unique()
+    tbl.string('make', 130).notNullable()
+    tbl.string('model', 130).notNullable()
+    tbl.numeric('mileage').unsigned().notNullable()
+    tbl.string('title', 130)
+    tbl.string('transmission', 130)
+  })
 };
 
 exports.down = function (knex) {
