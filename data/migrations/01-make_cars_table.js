38faf810f0f2ec739dbe5bbcc7dd9b787f3b6e57
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-23 08:48:04 -0500

    migrate and seed tested working

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -5,7 +5,7 @@ exports.up = function (knex) {
     tbl.string('vin', 17).notNullable().unique()
     tbl.string('make', 130).notNullable()
     tbl.string('model', 130).notNullable()
-    tbl.numeric('mileage').unsigned().notNullable()
+    tbl.integer('mileage').unsigned().notNullable()
     tbl.string('title', 130)
     tbl.string('transmission', 130)
   })
