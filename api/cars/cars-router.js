24d271f517aa8c0e1172360bfa6f901acef93bd7
David Tacoronte <d.tacoronte89@gmail.com>
2023-12-23 09:21:25 -0500

    router skeleton flushed out

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.json('getting all cars')
+})
+
+router.get('/:id', (req, res, next) => {
+    res.json(`get car by id ${req.params.id}`)
+})
+
+router.post('/', (req, res, next) => {
+    res.json('creating new car')
+})
+
+module.exports = router;
